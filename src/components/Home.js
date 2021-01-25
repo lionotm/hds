5b13e7a3d321e7bf70af373e383f2ca755d32519
Lionel Ong <lion@Lion-MBA.local>
2021-01-25 14:14:15 +0800

    update home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,15 @@ import React from 'react'
 
 function Home({ userName, signedIn }) {
   return (
-    signedIn ?
+    signedIn
+      ?
       <h2>
         Welcome {userName},
       </h2>
-      : ""
+      :
+      <h1>
+        Welcome to HDS! Start booking now.
+      </h1>
 
   )
 }
